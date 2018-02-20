e80a4a31cacf7a4b480b7966213ce607f2b51d62
Stepan Alekseev <stefan@hhh.gov>
2018-02-20 14:20:24 +0700

    main.c file added

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+  int i, j;
+  printf("Pered vami - shahmatnaya doska:\n");
+  for (i=0; i<=7; i++){ //* izmenenie stolbcov
+    for (j=0; j<=7; j++) { //* izmenenie strok
+      if(i%2==1) 
+        if (j%2==1) printf("  ");
+        else printf("*");
+      else 
+        if (j%2==1) printf("*");
+        else printf("  ");
+    }
+  printf("\n");
+  }
+}
