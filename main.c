f817162b1e540247c8b0828ae9ffd426d1f97f05
Stepan Alekseev <stefan@hhh.gov>
2018-02-26 19:56:48 +0700

    git ignore testing

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
   int i, j;
-  printf("Pered vami - shahmatnaya doska:\n");
+  printf("Pered vamii - shahmatnaya doska:\n");
   for (i=0; i<=7; i++){ //* izmenenie stolbcov
     for (j=0; j<=7; j++) { //* izmenenie strok
       if(i%2==1) 
